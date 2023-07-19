abdfe1b187eebc6f4a6bed4bc4492dec9a424ace
Md. Nuruzaman Milon <njmilon1@gmail.com>
2023-07-19 11:31:20 +0600

    test

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -9,13 +9,12 @@ const {
 } = require("../controllers/bookController");
 const bookRouter = express.Router();
 
-bookRouter.get("/", getAllBooks).post("/", ceateNewBook);
+bookRouter.get("/", getAllBooks);
+bookRouter.post("/", ceateNewBook);
 bookRouter.get("/dsc", getAllBooksDesc);
 bookRouter
   .get("/:id", getSingleBook)
   .patch("/:id", updateBook)
   .delete("/:id", deleteBook);
-// userRouter.delete("/:id", deleteUser)
-// userRouter.get("/create",createAllUserToDb)
 
 module.exports = bookRouter;
