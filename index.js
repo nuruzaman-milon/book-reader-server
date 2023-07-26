8f42bdb3bb2b1e9730768d27ba67e1bf607ce718
nuruzaman-milon <njmilon1@gmail.com>
2023-07-26 11:16:15 +0600

    cors added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const morgan = require("morgan");
 const bookRouter = require("./src/routes/bookRoutes");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
 
-// app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
